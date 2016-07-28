0a0a637dc56b30b6fd385820b93352b6a22b66ed
danypype <danypype@gmail.com>
2016-07-28 18:55:43 -0500

    Added function pokemongo.calculateMiddlePoint

--- pokemongo.js
+++ pokemongo.js
@@ -2,11 +2,16 @@
 
 var pokemongo = module.exports;
 
+pokemongo.constants = {
+  RESOLUTION: 100000,
+  FACTOR: 1 / 100000
+};
+
 pokemongo.calculatePokeTiles = function (centroid) {
   var centerLng = centroid[0];
   var centerLat = centroid[1];
-  var resolution = 100000;
-  var factor = 1 / resolution;
+  var resolution = this.constants.RESOLUTION;
+  var factor = this.constants.FACTOR;
 
   var centerPokeTile = {
     sw: [
@@ -27,5 +32,17 @@ pokemongo.calculatePokeTiles = function (centroid) {
     {sw: [(centerPokeTile.sw[0] - factor), (centerPokeTile.sw[1] - factor)]},
     {sw: [(centerPokeTile.sw[0]         ), (centerPokeTile.sw[1] - factor)]},
     {sw: [(centerPokeTile.sw[0] + factor), (centerPokeTile.sw[1] - factor)]}
+  ];
+};
+
+pokemongo.calculateMiddlePoint = function (pokeTile) {
+  var swLng = pokeTile.sw[0];
+  var swLat = pokeTile.sw[1];
+  var neLng = swLng + this.constants.FACTOR;
+  var neLat = swLat + this.constants.FACTOR;
+
+  return [
+    (swLng + neLng) / 2,
+    (swLat + neLat) / 2
   ];
 };
